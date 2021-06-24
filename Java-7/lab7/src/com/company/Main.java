510aaef0e273ceb1532c8737a879b7a0547fd585
BilikSergey <58261565+BilikSergey@users.noreply.github.com>
2021-06-24 12:47:10 +0300

    Add files via upload

---
+++ Java-7/lab7/src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import com.company.lab7.test.TestApp;
+import com.company.lab7.test.TestByConsole;
+import com.company.lab7.test.TestCollections;
+import com.company.lab7.test.TestFile;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        TestCollections testCollections = new TestCollections();
+        testCollections.main();
+       /* TestApp app = new TestApp();
+        app.startApp();*/
+        TestByConsole appConsole= new TestByConsole();
+        appConsole.startAppConsole();
+        TestFile.main();
+
+     //   TestStoreObject storeObject = new TestStoreObject();
+      //  storeObject.main();
+
+     //   TestRestoreObject restoreObject = new TestRestoreObject();
+      //  restoreObject.main();
+
+
+    }
+}
